6722b23cfc16162fb2a0a064f7a3139d9b3da0bf
kellinzapata-cyber <kellinzapata@gmail.com>
2026-08-26 19:06:51 -0500

    Refactor document list formatting in rules.py

--- services/rules.py
+++ services/rules.py
@@ -2,33 +2,71 @@ REGLAS = [
 
     {
         "nombre": "NIT",
-        "documentos": ["RUT", "FACTURA COMERCIAL"],
+        "documentos": [
+            "RUT",
+            "FACTURA COMERCIAL"
+        ],
         "campo_doc1": "nit",
         "campo_doc2": "nit"
     },
 
     {
         "nombre": "Razón Social",
-        "documentos": ["RUT", "FACTURA COMERCIAL"],
+        "documentos": [
+            "RUT",
+            "FACTURA COMERCIAL"
+        ],
         "campo_doc1": "razon_social",
         "campo_doc2": "razon_social"
     },
 
     {
         "nombre": "Dirección",
-        "documentos": ["RUT", "FACTURA COMERCIAL"],
+        "documentos": [
+            "RUT",
+            "FACTURA COMERCIAL"
+        ],
         "campo_doc1": "direccion",
         "campo_doc2": "direccion"
     },
 
-
     {
         "nombre": "Valor Factura",
-        "documentos": ["DUTA", "FACTURA COMERCIAL"],
+        "documentos": [
+            "DUTA",
+            "FACTURA COMERCIAL"
+        ],
         "campo_doc1": "valor_factura",
         "campo_doc2": "valor_factura"
     },
 
-    
+    {
+        "nombre": "Peso Bruto",
+        "documentos": [
+            "DUTA",
+            "BL"
+        ],
+        "campo_doc1": "peso_bruto",
+        "campo_doc2": "peso_bruto"
+    },
 
+    {
+        "nombre": "Peso Neto",
+        "documentos": [
+            "DUTA",
+            "BL"
+        ],
+        "campo_doc1": "peso_neto",
+        "campo_doc2": "peso_neto"
+    },
+
+    {
+        "nombre": "Usuario Zona Franca",
+        "documentos": [
+            "DUTA",
+            "BL"
+        ],
+        "campo_doc1": "usuario_zona_franca",
+        "campo_doc2": "usuario_zona_franca"
+    }
 ]
